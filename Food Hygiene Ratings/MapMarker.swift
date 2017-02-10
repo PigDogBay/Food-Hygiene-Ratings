e6acb55f374a90f013c175a36c91a4a7732c91fb
PigDogBay <pigdogbay@yahoo.co.uk>
2017-02-10 18:38:17 +0000

    Adjusted colours

--- Food Hygiene Ratings/MapMarker.swift
+++ Food Hygiene Ratings/MapMarker.swift
@@ -11,14 +11,14 @@ import MapKit
 
 class MapMarker : NSObject, MKAnnotation {
     
-    static let fiveStarColor = UIColor(colorLiteralRed: 0.0, green: 1.0, blue: 0, alpha: 1.0)
-    static let fourStarColor = UIColor(colorLiteralRed: 0.75, green: 1.0, blue: 0, alpha: 1.0)
-    static let threeStarColor = UIColor(colorLiteralRed: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+    static let fiveStarColor = UIColor(colorLiteralRed: 0.0, green: 0.8, blue: 0, alpha: 1.0)
+    static let fourStarColor = UIColor(colorLiteralRed: 0.4, green: 0.8, blue: 0, alpha: 1.0)
+    static let threeStarColor = UIColor(colorLiteralRed: 0.8, green: 0.8, blue: 0.0, alpha: 1.0)
 
-    static let twoStarColor = UIColor(colorLiteralRed: 1.0, green: 0.5, blue: 0.25, alpha: 1.0)
-    static let oneStarColor = UIColor(colorLiteralRed: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-    static let zeroStarColor = UIColor(colorLiteralRed: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-    
+    static let twoStarColor = UIColor(colorLiteralRed: 0.8, green: 0.4, blue: 0.0, alpha: 1.0)
+    static let oneStarColor = UIColor(colorLiteralRed: 0.8, green: 0.0, blue: 0.0, alpha: 1.0)
+    static let zeroStarColor = UIColor(colorLiteralRed: 0.4, green: 0.0, blue: 0.0, alpha: 1.0)
+
     let coordinate: CLLocationCoordinate2D
     let title : String?
     let subtitle: String?
