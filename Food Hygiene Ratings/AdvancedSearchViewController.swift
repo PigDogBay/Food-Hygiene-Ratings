b10f183f2555f514e94886d414b728f99a296865
PigDogBay <pigdogbay@yahoo.co.uk>
2017-02-23 21:09:58 +0000

    Wired up text fields

--- Food Hygiene Ratings/AdvancedSearchViewController.swift
+++ Food Hygiene Ratings/AdvancedSearchViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class AdvancedSearchViewController: UITableViewController {
+class AdvancedSearchViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var businessNameTextField: UITextField!
+    @IBOutlet weak var placeNameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,13 @@ class AdvancedSearchViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        businessNameTextField.delegate = self
+        placeNameTextField.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        businessNameTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,18 +34,17 @@ class AdvancedSearchViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField === businessNameTextField {
+            businessNameTextField.resignFirstResponder()
+            placeNameTextField.becomeFirstResponder()
+        } else if textField === placeNameTextField {
+            placeNameTextField.resignFirstResponder()
+            print("SEARCH!")
+        }
+        return true
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
