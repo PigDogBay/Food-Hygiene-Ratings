350240854201b4d47419537e728b294d36bb9f50
PigDogBay <pigdogbay@yahoo.co.uk>
2017-03-09 16:47:37 +0000

    Updated pods, removed InAppSettings

---
+++ Pods/Target Support Files/Pods-Food Hygiene Ratings-FoodHygieneRatingsTests/Pods-Food Hygiene Ratings-FoodHygieneRatingsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Food_Hygiene_Ratings_FoodHygieneRatingsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Food_Hygiene_Ratings_FoodHygieneRatingsTests
+@end
