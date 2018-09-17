4e5e37bc0728df7cef671cf8d77de5cdb30efbb7
PigDogBay <pigdogbay@yahoo.co.uk>
2018-09-17 12:37:58 +0100

    removed Pods tracking

--- Pods/Target Support Files/Pods-Food Hygiene Ratings-FoodHygieneRatingsTests/Pods-Food Hygiene Ratings-FoodHygieneRatingsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Food_Hygiene_Ratings_FoodHygieneRatingsTests : NSObject
-@end
-@implementation PodsDummy_Pods_Food_Hygiene_Ratings_FoodHygieneRatingsTests
-@end
