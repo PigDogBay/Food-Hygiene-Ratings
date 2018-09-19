819028cf6a12141fb3a45029030f0e25715ec592
PigDogBay <pigdogbay@yahoo.co.uk>
2018-09-19 10:45:56 +0100

    Added GooglePlaces API key

--- Food Hygiene Ratings/AppDelegate.swift
+++ Food Hygiene Ratings/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GMSPlacesClient.provideAPIKey("AIzaSyBT0jAib41UfT337euug5KUHE7waPQQeAE")
         let model = MainModel.sharedInstance
         let dataProvider = DataProvider()
 //        let dataProvider = DummyDataProvider()
