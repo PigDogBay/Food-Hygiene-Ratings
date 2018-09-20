abbe80df3082e8da1fa710ac41c5202e5dd2ee2e
PigDogBay <pigdogbay@yahoo.co.uk>
2018-09-20 13:15:08 +0100

    Moved places code into its own group

---
+++ Food Hygiene Ratings/GooglePlaceImage.swift
@@ -0,0 +1,9 @@
+//
+//  GooglePlaceImage.swift
+//  Food Hygiene Ratings
+//
+//  Created by Mark Bailey on 20/09/2018.
+//  Copyright © 2018 MPD Bailey Technology. All rights reserved.
+//
+
+import Foundation
--- Food Hygiene Ratings/MBPlace.swift
+++ Food Hygiene Ratings/MBPlace.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import GooglePlaces
+import UIKit
 
 enum FetchStatus {
     case uninitialized, fetching, ready, error
@@ -33,73 +33,4 @@ struct MBPlace {
     let images : [IPlaceImage]
 }
 
-class GooglePlaceFetcher : IPlaceFetcher {
-    private(set) var observableStatus: ObservableProperty<FetchStatus> = ObservableProperty<FetchStatus>(.uninitialized)
-    private(set) var mbPlace: MBPlace? = nil
-    
-    func fetch(establishment: Establishment) {
-        observableStatus.value = .fetching
-        let bounds = createBounds(establishment: establishment)
-        let filter = GMSAutocompleteFilter()
-        filter.type = .establishment
-        GMSPlacesClient.shared().autocompleteQuery(establishment.business.name, bounds: bounds, boundsMode: .restrict, filter: filter) { results,error in
-            if let error = error {
-                print("Autocomplete error \(error)")
-            }
-            if let results = results {
-                if results.count>0 {
-                    if let placeId = results[0].placeID{
-                        self.fetchPlace(placeId: placeId)
-                        return
-                    }
-                }
-            }
-            self.observableStatus.value = .error
-        }
-    }
-    private func fetchPlace(placeId : String){
-        GMSPlacesClient.shared().lookUpPlaceID(placeId) { result,error in
-            if let error = error {
-                print("Look up place by ID error \(error)")
-            }
-            if let place = result {
-                self.fetchImageMetadata(place: place)
-                return
-            }
-            self.observableStatus.value = .error
-        }
-    }
-    private func fetchImageMetadata(place : GMSPlace){
-        GMSPlacesClient.shared().lookUpPhotos(forPlaceID: place.placeID){ results,error in
-            if let error = error {
-                print("Look up place photos error \(error)")
-            }
-            if let results = results {
-                print("OK found \(results.results.count) pictures")
-                self.observableStatus.value = .ready
-                return
-            }
-            self.observableStatus.value = .error
-        }
 
-    }
-    
-    private func createPlace(place : GMSPlace, placeImages : [IPlaceImage]) -> MBPlace {
-        //convert the phone number from international format to UK
-        let phone = place.phoneNumber?.replacingOccurrences(of: "+44", with: "0") ?? ""
-        let web = place.website?.absoluteString ?? ""
-        return MBPlace(id: place.placeID, telephone: phone, web: web, images: placeImages)
-    }
-    
-    /*
-        Returns bounds of about 0.25 mile radius about the establishment
-    */
-    private func createBounds(establishment : Establishment) -> GMSCoordinateBounds{
-        let lat = establishment.address.coordinate.latitude
-        let long = establishment.address.coordinate.longitude
-        let delta = 0.005
-        let northeast = CLLocationCoordinate2D(latitude: lat+delta, longitude: long+delta)
-        let southwest = CLLocationCoordinate2D(latitude: lat-delta, longitude: long-delta)
-        return GMSCoordinateBounds(coordinate: northeast, coordinate: southwest)
-    }
-}
